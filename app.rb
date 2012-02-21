efb591b306b8fe6a3c569fcdcace26b136184086
Rene Hopf <renehopf@mac.com>
2012-02-21 12:02:33 +0100

    skip duplicates on import

--- app.rb
+++ app.rb
@@ -46,16 +46,24 @@ post "/upload" do
     gpx = Nokogiri::XML(File.open('uploads/'+params['myfile'][:filename])) # Open the gpx
     lat = (gpx.css('bounds')[0].values[0].to_f+gpx.css('bounds')[0].values[2].to_f)/2 #parse boundaries of query
     lon = (gpx.css('bounds')[0].values[1].to_f+gpx.css('bounds')[0].values[3].to_f)/2
-
+    old = 0
+    new = 0
     #add ALL the caches to the database
     gpx.css('wpt').each do |wpt|
-      Cache.create(:name => wpt.css('urlname').inner_text, #add cache to the database
+      cache = Cache.find_or_initialize_by_gccode(:name => wpt.css('urlname').inner_text, #add cache to the database
+                   :gccode => wpt.css('name').inner_text,
                    :lat  => wpt.values[0],
                    :lon  => wpt.values[1],
                    :gc_type => parse_type(wpt.css('type').inner_text).to_i)
+       if cache.persisted? #skip if already in db
+        old = old+1
+       else
+        new = new+1
+        cache.save #add new caches to db
+       end
     end
-   
-    return "The file was added to the database."
+
+    return "Importet #{new} caches, ignored #{old} duplicates"
 end
 
 get '/' do
