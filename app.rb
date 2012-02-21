a1d99d10a91e74998850caeb2f0572db6c7129a9
Rene Hopf <renehopf@mac.com>
2012-02-21 02:10:09 +0100

    http upload form fot gpx files

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'nokogiri'
 require 'json'
+require 'haml'
 
 class Cache < ActiveRecord::Base
 end
@@ -27,22 +28,31 @@ get '/pins.json' do
     Cache.select('name,lat,lon,gc_type').where("lat > #{params['lat2']} and lat < #{params['lat1']} and lon > #{params['lon2']} and lon < #{params['lon1']}").to_json
 end
 
-get '/parse' do
-  if File.exists?('pq.gpx') # If the gpx is available
-    gpx = Nokogiri::XML(File.open('pq.gpx')) # Open the gpx
-    lat = (gpx.css('bounds')[0].values[0].to_f+gpx.css('bounds')[0].values[2].to_f)/2 # And parse it into the coordinates...
+# Handle GET-request (Show the upload form)
+get '/upload' do
+    haml :upload
+end
+
+# Handle POST-request (Receive and save the uploaded file)
+post "/upload" do
+    #save file to uploads folder
+    File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+        f.write(params['myfile'][:tempfile].read)
+    end
+
+    gpx = Nokogiri::XML(File.open('uploads/'+params['myfile'][:filename])) # Open the gpx
+    lat = (gpx.css('bounds')[0].values[0].to_f+gpx.css('bounds')[0].values[2].to_f)/2 #parse boundaries of query
     lon = (gpx.css('bounds')[0].values[1].to_f+gpx.css('bounds')[0].values[3].to_f)/2
 
-    #all ALL the caches to the database
+    #add ALL the caches to the database
     gpx.css('wpt').each do |wpt|
       Cache.create(:name => wpt.css('urlname').inner_text, #add cache to the database
                    :lat  => wpt.values[0],
                    :lon  => wpt.values[1],
                    :gc_type => parse_type(wpt.css('type').inner_text).to_i)
     end
-  else
-    "Pocket Query existiert nicht, oder ist nicht lesbar." # Else fail
-  end
+   
+    return "The file was added to the database."
 end
 
 get '/' do
